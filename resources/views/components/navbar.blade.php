bd3056bf22a4ace596331cade0e1147fcdcef75e
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-11-02 05:19:13 +0330

    [feat] add authentication

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,113 @@
+<nav class="bg-white border-gray-200 dark:bg-gray-900" style="background-color: #393939">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+        <a href="#" class="flex items-center">
+            <span
+                class="self-center text-2xl font-semibold whitespace-nowrap  text-blue-700">{{Auth::user()->name}}</span>
+        </a>
+        <div class="flex items-center md:order-2">
+            <button type="button" class="flex mr-3 text-sm border border-[#152737] p-2 rounded-full md:mr-0"
+                    id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+                    data-dropdown-placement="bottom">
+                <p class="text-green-400 text-xl">پنل</p>
+                {{--                <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">--}}
+            </button>
+            <!-- Dropdown menu -->
+            <div
+                class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                id="user-dropdown">
+                <div class="px-4 py-3">
+                    <span class="block text-sm text-gray-900 dark:text-white">ایمیل</span>
+                    <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                        {{Auth::user()->email}}
+                    </span>
+                </div>
+                <ul class="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                        <a href="{{route('dashboard')}}"
+                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">داشبورد</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">پروفایل</a>
+                    </li>
+                    @role('admin')
+                        <li>
+                            <div id="doubleDropdown"
+                                 class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                    aria-labelledby="doubleDropdownButton">
+                                    <li>
+                                        <a href="{{route('restaurantCategories.index')}}"
+                                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">دسته
+                                            بندی رستوران</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('foodCategories.index')}}"
+                                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">دسته
+                                            بندی غذا</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown"
+                                    data-dropdown-placement="right-start" type="button"
+                                    class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                مدیریت دسته بندی ها
+                                <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                     fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                            </button>
+                        </li>
+                    @endrole
+                    @role('seller')
+                    @if(Auth::user()->restaurant)
+                        <li>
+                            <a href="{{route('restaurants.show', Auth::user()->restaurant->id)}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">تنظیمات رستوران</a>
+                        </li>
+                    @endif
+
+
+                    @endrole
+
+                    <li>
+                        <form action="{{route('logout')}}" method="post"
+                              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            @csrf
+                            <button type="submit">خارج شدن</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+            <button data-collapse-toggle="navbar-user" type="button"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-user" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                     viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+        </div>
+        {{--        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">--}}
+        {{--            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">--}}
+        {{--                <li>--}}
+        {{--                    <a href="#" class="block py-2 pl-3 pr-4 text-black  rounded md:p-0 md:dark:text-blue-500" aria-current="page">خانه</a>--}}
+        {{--                </li>--}}
+        {{--                <li>--}}
+        {{--                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>--}}
+        {{--                </li>--}}
+        {{--                <li>--}}
+        {{--                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>--}}
+        {{--                </li>--}}
+        {{--                <li>--}}
+        {{--                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>--}}
+        {{--                </li>--}}
+        {{--                <li>--}}
+        {{--                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>--}}
+        {{--                </li>--}}
+        {{--            </ul>--}}
+        {{--        </div>--}}
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::middleware('auth')->group(function () {
+    Route::get('home',[PageController::class, 'home'])->name('home');
+    Route::post('dashboard',[AuthController::class, 'logout'])->name('logout');
+    Route::get('dashboard',[PageController::class, 'dashboard'])->name('dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
+});
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthController::class, 'showLogin'])
+        ->name('show.login');
+    Route::post('login', [AuthController::class, 'login'])
+        ->name('login');
+    Route::get('register', [AuthController::class, 'showRegister'])
+        ->name('show.register');
+    Route::post('register', [AuthController::class, 'register'])
+        ->name('register');
+    Route::get('/',[PageController::class, 'welcome']);
 });
