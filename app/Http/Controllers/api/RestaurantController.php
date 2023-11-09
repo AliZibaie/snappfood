206149ed414047557675c94238c0474f1cff03ad
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-11-10 02:01:38 +0330

    feat: add filter restaurant by category, is open and score

--- app/Http/Controllers/api/RestaurantController.php
+++ app/Http/Controllers/api/RestaurantController.php
@@ -8,17 +8,18 @@ use App\Http\Resources\Address\AddressResource;
 use App\Http\Resources\Restaurant\RestaurantCollection;
 use App\Http\Resources\Restaurant\RestaurantResource;
 use App\Models\Address;
-use App\Models\Restaurant;
+use App\Models\Restaurant as Model;
+use App\Services\resources\Restaurant;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        return new RestaurantCollection(Restaurant::all());
+        return new RestaurantCollection(Restaurant::filter());
     }
 
-    public function show(Restaurant $restaurant)
+    public function show(Model $restaurant)
     {
         return new RestaurantResource($restaurant);
     }
--- app/Services/resources/Restaurant.php
+++ app/Services/resources/Restaurant.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\resources;
 
+use App\Models\RestaurantCategory;
+
 class Restaurant
 {
     public static function format(array $schedules) : array
@@ -12,5 +14,29 @@ class Restaurant
             $newFormat[$schedule['day']] = ['start'=>$schedule['open'], 'end'=>$schedule['close']];
         }
         return $newFormat;
+    }
+
+    public static function filter()
+    {
+        $isOpen = request('is_open');
+        $type = request('type');
+        $scoreGreaterThan = request('score');
+//        dd($isOpen, $type, $scoreGreaterThan);
+        $query = \App\Models\Restaurant::query();
+
+        if (isset($isOpen)) {
+            $query->where('status', $isOpen);
+        }
+
+        if ($type) {
+            $id = RestaurantCategory::query()->where('restaurant_type', $type)->first()->id;
+            $query->where('restaurant_category_id', $id);
+        }
+
+        if ($scoreGreaterThan) {
+            $query->where('score', '>', $scoreGreaterThan);
+        }
+
+        return $query->get();
     }
 }
